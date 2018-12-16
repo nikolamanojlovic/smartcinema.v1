eec3839cdf122996d35b07b2de216fb75b3783ea
Nikola Manojlović <manojlovic.nikola95@gmail.com>
2018-12-16 13:57:52 +0100

    First commit for Git

---
+++ src/main/java/com/nmanojlovic/smartcinema/SmartcinemaApplication.java
@@ -0,0 +1,14 @@
+package com.nmanojlovic.smartcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcinemaApplication.class, args);
+    }
+
+}
+
