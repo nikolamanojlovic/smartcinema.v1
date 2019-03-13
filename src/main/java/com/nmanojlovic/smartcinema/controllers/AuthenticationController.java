b3c080fd4fb2a61c516539eb99db3fc29edd3347
Nikola Manojlović <manojlovic.nikola95@gmail.com>
2019-03-13 22:06:48 +0100

    Fixed

--- src/main/java/com/nmanojlovic/smartcinema/controllers/AuthenticationController.java
+++ src/main/java/com/nmanojlovic/smartcinema/controllers/AuthenticationController.java
@@ -1,14 +1,14 @@
 package com.nmanojlovic.smartcinema.controllers;
 
-import com.nmanojlovic.smartcinema.data.LogInData;
 import com.nmanojlovic.smartcinema.models.User;
 import com.nmanojlovic.smartcinema.services.IUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Optional;
 
 @RestController("/auth")
 public class AuthenticationController {
