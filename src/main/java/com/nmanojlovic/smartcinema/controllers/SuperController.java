9e051ad89d717b72fb9c068e410724a93133fe06
Nikola Manojlović <manojlovic.nikola95@gmail.com>
2019-04-21 10:31:03 +0200

    JSON encoding fixed

--- src/main/java/com/nmanojlovic/smartcinema/controllers/SuperController.java
+++ src/main/java/com/nmanojlovic/smartcinema/controllers/SuperController.java
@@ -1,21 +1,24 @@
 package com.nmanojlovic.smartcinema.controllers;
 
 import com.google.gson.Gson;
+import com.nmanojlovic.smartcinema.constants.Constants;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.Optional;
 
-public class SuperController {
+public abstract class SuperController {
 
     @Resource
     private Gson gson;
 
     protected <T> ResponseEntity<String> sendResponse(Optional<T> data, Class dataType, HttpStatus fail) {
-        if ( data.isPresent() ) {
-            return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(data.get(), dataType));
+        if (data.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .body(gson.toJson(data.get(), dataType));
         }
         return ResponseEntity.status(fail).body(null);
     }
