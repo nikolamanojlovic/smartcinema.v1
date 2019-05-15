4482da5ffac13e3bd2d613900db0399b80ddde2e
nikolamanojlovic <manojlovic.nikola95@gmail.com>
2019-05-15 16:17:10 +0200

    Changes for macos

--- src/main/java/com/nmanojlovic/smartcinema/SmartCinemaApplication.java
+++ src/main/java/com/nmanojlovic/smartcinema/SmartCinemaApplication.java
@@ -2,10 +2,11 @@ package com.nmanojlovic.smartcinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(
         exclude = {
-                org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+                SecurityAutoConfiguration.class,
         }
 )
 public class SmartCinemaApplication {
