8d78ad22de8e15ba6d11f90d6bed305c32d8ffe6
nmanojlovic <n.manojlovic@msgnetconomy.net>
2019-06-13 14:06:06 +0200

    Pagination menu

--- reactsrc/src/pages/homePage.js
+++ reactsrc/src/pages/homePage.js
@@ -3,10 +3,35 @@ import {connect} from "react-redux";
 import FilmList from "../components/filmList";
 import SideBar from "../components/sideBar";
 import {CreateTicketForCurrentUser} from "../functions/ticketFunctions";
+import Button from "@material-ui/core/Button";
+import {NavigateBefore, NavigateNext} from "@material-ui/icons";
+
+const styles = {
+    pagination: {
+        marginLeft: "15%",
+        marginBottom: 20,
+        textAlign: "center"
+    },
+    buttonStyle: {
+        color: "#FFFFFF",
+        backgroundColor: "#A5122C",
+        width: 20,
+        margin: 5
+    },
+    buttonCurrent: {
+        width: 20,
+        color: "#A5122C",
+        backgroundColor: "#FFFFFF"
+    }
+};
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            currentPage: 1
+        };
     };
 
     componentDidMount() {
@@ -15,11 +40,32 @@ class HomePage extends Component {
         }
     }
 
+    _handlePageChangeNext(page, event) {
+        this.setState({currentPage: page + 1})
+    }
+
+    _handlePageChangePrevious(page, event) {
+        this.setState({currentPage: page -1})
+    }
+
     render() {
-        return(
+        return (
             <div>
                 <SideBar/>
-                <FilmList/>
+                <FilmList pagination={this.state.currentPage}/>
+                <div style={styles.pagination}>
+                    <Button variant="contained" className="previous" style={styles.buttonStyle} disabled={this.state.currentPage === 1}
+                            onClick={(event) => this._handlePageChangePrevious(this.state.currentPage, event)}>
+                        <NavigateBefore/>
+                    </Button>
+                    <Button variant="contained" className="current" style={styles.buttonCurrent} disabled={true}>
+                        {this.state.currentPage}
+                    </Button>
+                    <Button variant="contained" className="next" style={styles.buttonStyle}
+                            onClick={(event) => this._handlePageChangeNext(this.state.currentPage, event)}>
+                        <NavigateNext/>
+                    </Button>
+                </div>
             </div>
         );
     }
